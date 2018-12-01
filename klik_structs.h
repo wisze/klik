29541fddadd787e5a70d16dadf7c0b3f7b884c28
Edwin <edwin@wisze.net>
2018-12-01 22:08:13 +0100

    Renamed

---
+++ klik_structs.h
@@ -0,0 +1,58 @@
+/**
+ * Time and date stuff
+ **/
+struct DateTime {
+  int sec;
+  int dsec; // seconds since midnight
+  int min;
+  int hour;
+  int dow;
+  int day;
+  int month;
+  int year;
+  int config;
+  int doy;    // not BCD!
+};
+
+/**
+ * Automatic switch
+ */
+struct SwitchCommand {
+  int no;
+  int whenOn;   // When to switch in seconds since midnight
+  int whenOff;  // When to switch in seconds since midnight
+  boolean on;
+};
+
+struct Switch {
+  int no;
+  String name;
+};
+
+/**
+ * Sensorthings stuff
+ */
+struct Thing {
+  String name;
+  String description;
+};
+
+struct UnitofMeasurement {
+  String name;
+  String symbol;
+  String uri;
+};
+
+struct Datastream{
+  String name;
+  String description;
+  String observationtype;
+  UnitofMeasurement unit;
+};
+
+struct Observation {
+  float result;
+};
+
+
+
